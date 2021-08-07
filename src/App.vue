298bc5f98807a6498e244cdca5bafad548571021
Katsuragawa <andre.katsuragawa@gmail.com>
2021-08-07 20:26:27 -0300

    Add form and toggle button

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
+    AddTask,
   },
   data() {
     return {
       tasks: [],
+      showAddTask: false,
     };
+  },
+  methods: {
+    deleteTask(id) {
+      if (confirm('Sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      const [taskToToggle] = this.tasks.filter((task) => task.id === id);
+      taskToToggle.reminder = !taskToToggle.reminder;
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+		toggleAddTask() {
+			this.showAddTask = !this.showAddTask
+		}
   },
   created() {
     this.tasks = [
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,37 @@
 <template>
-	<header>
-		<h1>{{title}}</h1>
-		<Button text="Add Task" color="green" />
-	</header>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
 </template>
 
 <script>
-	import Button from './Button'
+import Button from './Button';
 
-	export default {
-		name: 'Header',
-		props: {
-			title: {
-				type: String,
-				default: "Hello World"
-			}
-		},
-		components: {
-			Button
-		}
-	}
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      default: 'Hello World',
+    },
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+};
 </script>
 
 <style scoped>
-	header {
-		display: flex;
-		justify-content: space-evenly;
-		align-items: center;
-		margin-bottom: 20px;
-	}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
 </style>
